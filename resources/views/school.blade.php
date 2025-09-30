abb1f5386b082cf27a4e0eafaae7d7355f95847e
Elijah Israel <elijahisrael@Elijahs-MacBook-Air.local>
2025-09-30 23:40:21 +0300

    add root redirect to ketiai landing page

--- resources/views/school.blade.php
+++ resources/views/school.blade.php
@@ -1,7 +1,60 @@
 @extends('layouts.base')
 
 @section('content')
-    <div class="container">
-        <h1>Dashboard</h1>
+    <div class="row">
+        <div class="col-lg-3 col-sm-6">
+            <div class="card">
+                <div class="stat-widget-two card-body">
+                    <div class="stat-content">
+                        <div class="stat-text">Students </div>
+                        <div class="stat-digit"> 0</div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-success w-85" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card">
+                <div class="stat-widget-two card-body">
+                    <div class="stat-content">
+                        <div class="stat-text">Appointments</div>
+                        <div class="stat-digit">0</div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-primary w-75" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card">
+                <div class="stat-widget-two card-body">
+                    <div class="stat-content">
+                        <div class="stat-text">Lab Tests</div>
+                        <div class="stat-digit"> 0</div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-warning w-50" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card">
+                <div class="stat-widget-two card-body">
+                    <div class="stat-content">
+                        <div class="stat-text">Doctors</div>
+                        <div class="stat-digit"> 0</div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-danger w-65" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- /# card -->
+        </div>
+        <!-- /# column -->
     </div>
 @endsection
