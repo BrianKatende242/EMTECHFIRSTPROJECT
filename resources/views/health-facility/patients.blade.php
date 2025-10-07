645b2319611fe14cbfca081d213cf4849f2f28ac
Elijah Israel <mukisaelijah293@gmail.com>
2025-10-07 23:14:46 +0300

    style(patients): render success flash in black using alert-light text-dark

--- resources/views/health-facility/patients.blade.php
+++ resources/views/health-facility/patients.blade.php
@@ -10,7 +10,7 @@
     </div>
 
     @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
+        <div class="alert alert-light text-dark">{{ session('success') }}</div>
     @endif
     @if(session('error'))
         <div class="alert alert-danger">{{ session('error') }}</div>
